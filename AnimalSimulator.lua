0e29432e12584ace11b63bddf2947ced2c605c21
Mc4121ban <105984396+Mc4121ban@users.noreply.github.com>
2023-05-16 21:15:59 +0700

    Update AnimalSimulator.lua

--- AnimalSimulator.lua
+++ AnimalSimulator.lua
@@ -1,1 +1,5 @@
+_G.RedGUI = false
+_G.Theme = "Dark"
+--Themes: Light, Dark, Mocha, Aqua and Jester
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/AnimalSimulator.lua"))()
